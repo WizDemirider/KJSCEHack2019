96f4272f509b81614e37bb706bd36eb6b6272c15
Ankit Gupta <argankit@gmail.com>
2019-10-05 16:09:20 +0530

    basic django login signup

---
+++ QPaper/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('login', views.loginUser, name="login"),
+    path('logout', views.logoutUser, name="logout"),
+    path('signup', views.signupUser, name="signup"),
+    path('home', views.home, name="home"),
+]
---
+++ QPaper/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.utils.html import escape
+
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return HttpResponse("Welcome!")
+
+def signupUser(request):
+    if request.method == 'POST':
+        username = escape(request.POST['username'])
+        raw_password = escape(request.POST['password1'])
+        raw_password2 = escape(request.POST['password2'])
+        if raw_password == raw_password2:
+            user = User.objects.create(username=username, password=raw_password)
+            user.set_password(raw_password)
+            user.save()
+            login(request, user) # logs User in
+            return redirect('home')
+    return render(request, 'Authentication/signup.html')
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = escape(request.POST['username'])
+        raw_password = escape(request.POST['password'])
+        user = authenticate(username=username, password=raw_password)
+        if user is not None:
+            login(request, user) # logs User in
+            return redirect('home')
+    return render(request, 'Authentication/login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
+
+@login_required
+def home(request):
+    return HttpResponse("your dashboard")
